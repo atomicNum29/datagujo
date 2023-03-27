23bd25d43e8a1c9b1aa32ddbb5449aa37188a956
atomicNum29 <atomicw63.546@gmail.com>
2023-03-27 15:50:05 +0900

    3월 27일 2교시 끝.

--- datagujo/0321_linkedList.c
+++ datagujo/0321_linkedList.c
@@ -64,7 +64,25 @@ void addToSLL(int _v) {
 	return;
 }
 
-void displaySLL() {
+// _v를 저장한 노드를 맨 앞에 추가.
+void addToFront(int _v) {
+
+	struct node* _new = (struct node*)malloc(sizeof(struct node));
+	_new->data = _v;
+	_new->next = NULL;
+
+	// 아무것도 없을 때
+	if (head == NULL) {
+		head = _new;
+		return;
+	}
+	// 이미 뭔가 있을 때
+	_new->next = head; // 새친구가 먼저 접근하기.
+	head = _new;
+	return;
+}
+
+void displaySLL(void) {
 	
 	struct node* temp = head;
 	while (temp) {
@@ -103,11 +121,90 @@ struct node* findSLL(int _v) {
 	return NULL;
 }
 
+// _findv값을 가진 노드를 찾아서 그 뒤에 _addv 노드를 추가.
+void insertInto(int _findv, int _addv) {
+
+	struct node* target = findSLL(_findv);
+	if (target == NULL) {
+		return;
+	}
+
+	struct node* _new = (struct node*)malloc(sizeof(struct node));
+	_new->data = _addv;
+	_new->next = NULL; // 이 부분은 노드를 생성하고 초기화 하는 구역이다.
+	// 각 구역에서 하는 일을 확실히 구분하며, 
+	// 해당 구역에서 하는 일에만 충실하게 작성하는 것이 좋다.
+
+	_new->next = target->next;
+	target->next = _new;
+	return;
+}
+
+int delFromFront(void) {
+
+	if (head == NULL) {
+		return -999; // error code
+	}
+
+	// 목표물 포착
+	struct node* spear = head;
+	// 주민 대피
+	head = head->next;
+	// 인질 구출
+	int rtn = spear->data;
+	// 목표물 파괴
+	free(spear);
+	// 인질 귀가
+	return rtn;
+}
+
+int delFromLast(void) {
+
+	if (head == NULL) { // 비었을 때
+		return -999; // error code
+	}
+
+	if (head->next == NULL) { // 노드가 한 개일때
+		int rtn = head->data;
+		free(head);
+		head = NULL;
+		return rtn;
+	}
+
+	struct node* spear = head;
+	while (spear->next->next) {
+		spear = spear->next;
+	}
+	int rtn = spear->next->data;
+	free(spear->next);
+	spear->next = NULL;
+	return rtn;
+}
+
+// SLL의 모든 노드를 삭제한다.
+void destroySLL(void) {
+	
+	struct node* spear = head;
+
+	while (head) {
+		head = head->next;
+		free(spear);
+		spear = head;
+	}
+	return;
+}
+
 int main() {
 
 	addToSLL(10);
 	addToSLL(20);
 	addToSLL(30);
+
+	addToFront(90);
+	addToFront(80);
+
+	printf("앞에서 삭제한 노드의 data: %d\n", delFromFront());
+	printf("뒤에서 삭제한 노드의 data: %d\n", delFromLast());
 
 	displaySLL();
 
@@ -115,14 +212,16 @@ int main() {
 
 	printf("20을 저장한 노드의 주소: %p\n", findSLL(20));
 
-	// addToFront 맨 앞 노드 추가
-	// delFromLast 맨 뒤 노드 제거
-	// delFromFront 맨 앞 노드 제거
-	// insertInto 특정 노드 뒤에 추가
+	destroySLL();
+
+	//* addToFront 맨 앞 노드 추가
+	//* delFromLast 맨 뒤 노드 제거
+	//* delFromFront 맨 앞 노드 제거
+	//* insertInto 특정 노드 뒤에 추가
 	// delFromSLL 특정 원소 삭제
-	// destroySLL 리스트 전체 삭제
-	// countSLL 노드 수 세기
-	// findSLL 특정 원소의 인덱스 찾기
+	//* destroySLL 리스트 전체 삭제
+	//* countSLL 노드 수 세기
+	//* findSLL 특정 원소의 인덱스 찾기
 	//
 
 	return 0;
