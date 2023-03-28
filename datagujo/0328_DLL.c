e767f913f99f5517b9499390d78625516a625f0d
atomicNum29 <atomicw63.546@gmail.com>
2023-03-28 16:29:47 +0900

    3월 28일 DLL구현 초기 커밋.

---
+++ datagujo/0328_DLL.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//
+// doubly linked list
+// next, prev
+// head
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+	int data;
+	struct node* next;
+	struct node* prev;
+};
+
+int main() {
+
+	return 0;
+}
